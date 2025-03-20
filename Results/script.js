7e516b38dcccd5bacf552cf94ac4a1e62d2dfc15
ahammed-ajmal <ah.ajmal.pp@gmail.com>
2025-03-20 14:03:34 +0530

    link

--- Results/script.js
+++ Results/script.js
@@ -1,5 +1,5 @@
 // const apiUrl = "https://script.google.com/macros/s/AKfycbwmxklwEAhoW2E4cP2skLjFrfBLFgKkwN9UfPCcTAwY5aeX4PHA2TWfEWsqKhHFll9qTQ/exec";
-const apiUrl = "https://script.google.com/macros/s/AKfycbzBx3XmBBLlWDKm_i8ca3dzEmd6ZAcGlaL4su4D6eXVTifT12edqtOsAeC0mqnpl-W-QA/exec";
+const apiUrl = "https://script.google.com/macros/s/AKfycbzroDYI3q2N2gk0K3afzNdl3e4ztP0dwe8BDZEdKVKDgT9r7ilZ-mK4_9FoTIHS1k4EIA/exec";
 
 function fetchStudentData(event) {
     event.preventDefault();
